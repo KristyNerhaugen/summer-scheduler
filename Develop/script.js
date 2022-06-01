747e0cc3192cf66688881e6aa684314d71ccdd91
kristynerhaugen <kristynerhaugen@gmail.com>
2022-06-01 12:10:26 -0500

    updated pseudocode steps

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,18 @@
 // when planner is onpened then current day is displayed 
 var rightNow = moment().format("MMMM Do, YYYY");
 console.log(rightNow);
+//get date to display on the top of the page
+
 // upon scrolling time blocks for standard business hours appear
+
 // when time blocks are viewed
 // each time block has color-code to indicate if it's in the past, present, or future
-// upon 'click' event for time block
+
+// upon 'click' for time block
 // then an event can be entered
+
 // upon save 'click' 
 // then text for that event is saved in local storage
+
 // when page is refresehd
 // then the saved events persist 
