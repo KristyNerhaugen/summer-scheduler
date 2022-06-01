14dd701061eea6fb5aeee916ff2e9b9d30e5ada4
kristynerhaugen <kristynerhaugen@gmail.com>
2022-06-01 10:48:52 -0500

    added script.js and wrote out acceptance criteria

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+// when planner is onpened
+// then current day is displayed (moment.js)
+// upon scrolling time blocks for standard business hours appear
+// when time blocks are viewed
+// each time block has color-code to indicate if it's in the past, present, or future
+// upon 'click' event for time block
+// then an event can be entered
+// upon save 'click' 
+// then text for that event is saved in local storage
+// when page is refresehd
+// then the saved events persist 
