60146e54e1228ded90ddb087be91a8dc77559359
kristynerhaugen <kristynerhaugen@gmail.com>
2022-06-02 12:42:35 -0500

    trying to save tasks

--- Develop/script.js
+++ Develop/script.js
@@ -4,29 +4,37 @@ $( document ).ready(function() {
 var rightNow = moment().format("MMMM Do, YYYY");
 // var currentDay = document.querySelector("#currentDay")
 // currentDay.innerHTML = rightNow
-
+// when planner is opened then current day is displayed on the top of the page (used moment.js)
 $("#currentDay").html(rightNow)
-// when planner is onpened then current day is displayed (used moment.js)
-console.log(rightNow);
-
-//get date to display on the top of the page
-
-// upon scrolling time blocks for standard business hours appear
 
 // when time blocks are viewed
 // each time block has color-code to indicate if it's in the past, present, or future
+// empty array to store tasks 
+var tasks = [];
+console.log(tasks);
 
 // upon 'click' for time block
-// then an event can be entered
+// then an event can be entered 
 
 // upon save 'click' 
 // then text for that event is saved in local storage
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
 var text = "event"
 localStorage.setItem("task", tasks)
 
 localStorage.setItem("8am", "8am tasks")
 localStorage.setItem("9am", "9am tasks")
-
+localStorage.setItem("10am", "10am tasks")
+localStorage.setItem("11am", "11am tasks")
+localStorage.setItem("12pm", "12pm tasks")
+localStorage.setItem("1pm", "1pm tasks")
+localStorage.setItem("2pm", "2pm tasks")
+localStorage.setItem("3pm", "3pm tasks")
+localStorage.setItem("4pm", "4pm tasks")
+localStorage.setItem("5pm", "5pm tasks")
 
 // when page is refresehd
 // then the saved events persist 
